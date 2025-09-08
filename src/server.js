84a51b4d8e585b2ff485b99130a03f9833bc4c95
Denilson valero <denilsonvalero@gmail.com>
2025-09-08 13:48:03 -0300

    base local conectada sigue con un error

--- src/server.js
+++ src/server.js
@@ -1,9 +1,26 @@
-const express = require ("express")
+const express = require("express")
+import express from "express"
+const msql = require ("mysql2")
 const server= express()
 const port = 8080
 
 server.use(express.json());
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Denigvm10",
+    database: "todo_db"
+});
+
+db.connect((err) =>{
+    if (err){
+        console.log("Error al conectar", err);
+        return;
+    }
+    console.log("base conectada");
+    
+});
 server.get("/",(req, res) => {
      res.send("Servidor funcionando")
 });
