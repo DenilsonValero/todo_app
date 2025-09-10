7a4e086745addf3b7df8bb038b07ab88ed50b4c3
Denilson valero <denilsonvalero@gmail.com>
2025-09-10 16:13:18 -0300

    agregado codigo para selecionar todas la tareas y para agregar las tareas a traves de la base de datos

--- server.js
+++ server.js
@@ -20,11 +20,42 @@ db.connect((err) =>{
     console.log("base conectada");
     
 });
+
+server.get("/tareas", (req,res) =>{
+    db.query("SELECT * FROM todo_db.tareas;", (err, results)=>{
+        if (err) return res.status(500).json(err);
+        res.json(results);
+    });
+});
+
+server.post("/tareas",(req,res) =>{
+   
+    const {tareascol} = req.body;
+    if (!tareascol) {
+        console.log("no lego la tarea");
+        
+      return res.status(400).json({error:"falta informacion"})
+    }
+
+    db.query("INSERT INTO tareas (tareascol) VALUES (?)",
+    [tareascol],
+    (err,result) =>{
+    if (err)
+        console.log("error en el query",err);
+        
+        {return res.status(500).json(err);}
+
+    return res.status(201).json({id: result.insertId, tareascol, completada: false});
+});
+});
+
+
+
 server.get("/",(req, res) => {
      res.send("Servidor funcionando")
 });
 
 server.listen(PORT, () => {
-    console.log(`Servidor iniciando en //localhot${PORT}`);
+    console.log(`Servidor iniciando en http://localhost:${PORT}`);
     
 }); 
